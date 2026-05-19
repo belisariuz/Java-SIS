f439e9e2a669e81d7d352a5c90b87eeb7c51fb87
YYoussefahmeDD <youssefyoussefyy431@gmail.com>
2026-05-19 08:51:56 +0300

    user 1st Extra department finish 1

--- project/src/Course.java
+++ project/src/Course.java
@@ -3,6 +3,8 @@ import java.util.List;
 public class Course {
 
 
+
+
     private String courseId;
     private String title;
     private String description;
@@ -94,9 +96,121 @@ public class Course {
     public void displayCourseDetails (){
         System.out.println("Course details : \n");
         System.out.println("Course code/ID : " + courseId );
-        System.out.println("Course code/ID : " + courseId );
-
+        System.out.println("Title : " + title );
+        System.out.println("Description : " + description);
+        System.out.println("Credit hours : " + creditHours);
+        System.out.println(" Prerequisites: " + (prerequisites  != null ? prerequisites : "No prerequisites needed"));
+        System.out.println("Course Instructor name: " + instructorName);
+        System.out.println("Course Instructor ID: " + instructorId);
+        System.out.println("Schedule : " + schedule);
+        System.out.println("Location and room assigned : " + roomAssigned);
+        System.out.println("Semster : " + semster);
+        System.out.println("Enrolled students = " + enrolledStudents);
+        System.out.println("Max capacity = " + maxCapacity);
+        System.out.println("Seats left = " + (maxCapacity - enrolledStudents);
     }
+
+    //etters
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public List<String> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public String getLabAssigned() {
+        return labAssigned;
+    }
+
+    public String getRoomAssigned() {
+        return roomAssigned;
+    }
+
+    public String getSchedule() {
+        return schedule;
+    }
+
+    public String getSemster() {
+        return semster;
+    }
+
+    public int getCurrentlyEnrolled() {
+        return currentlyEnrolled;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public String getInstructorId() {
+        return instructorId;
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public String getPrerequisites() {
+        return prerequisites;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    // Setters
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setSemster(String semster) {
+        this.semster = semster;
+    }
+
+    public void setPrerequisites(String prerequisites) {
+        this.prerequisites = prerequisites;
+    }
+
+    public void setCreditHours(int creditHours) {
+        this.creditHours = creditHours;
+    }
+
+    public void setSchedule(String schedule) {
+        this.schedule = schedule;
+    }
+
+    public void setRoomAssigned(String roomAssigned) {
+        this.roomAssigned = roomAssigned;
+    }
+
+    public void setInstructorId(String instructorId) {
+        this.instructorId = instructorId;
+    }
+
+
+
+
+
 
 
 }
