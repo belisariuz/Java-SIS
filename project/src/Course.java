ae24d3f8bf14f3af00072fba148ffc8ecffb57b5
YYoussefahmeDD <youssefyoussefyy431@gmail.com>
2026-05-19 07:02:22 +0300

    Course.java part 1

--- project/src/Course.java
+++ project/src/Course.java
@@ -1,24 +1,103 @@
+import java.util.ArrayList;
 import java.util.List;
 public class Course {
-    String courseid;
-     String title;
-     String description;
-     int credithours;
-     String prerequisites;
-     int maxcapacity;
-     String instructorid;
-     String schedule;
-     String coursetype;
-    String enrolledstudentId;
-    public void addStudent(String studentId) {
-        enrolledstudentId = studentId;
+
+
+    private String courseId;
+    private String title;
+    private String description;
+    private int creditHours;
+    private String prerequisites;
+    private String instructorName;
+    private String instructorId;
+    private String departmentId;
+    private int maxCapacity;
+    private int currentlyEnrolled;
+    private String semster;
+    private String schedule;
+    private String roomAssigned;
+    private String labAssigned;
+    private List <String> enrolledStudents;
+
+
+
+    // constructor
+
+    public Course( String courseId , String title , String description , int creditHours , String prerequisites ,
+                   String instructorName , String instructorId , String departmentId ,int maxCapacity , int currentlyEnrolled ,
+                   String semster , String schedule , String roomAssigned , String labAssigned) {
+        this.courseId = courseId ;
+        this.title = title ;
+        this.description = description ;
+        this.creditHours = creditHours;
+        this.prerequisites = prerequisites;
+        this.instructorName = instructorName;
+        this.instructorId = instructorId;
+        this.departmentId =departmentId;
+        this.maxCapacity = maxCapacity;
+        this.currentlyEnrolled = currentlyEnrolled;
+        this.semster = semster;
+        this.schedule = schedule;
+        this.roomAssigned = roomAssigned;
+        this.labAssigned = labAssigned;
+        this.enrolledStudents = new ArrayList<>();
     }
-    public void removeStudent(String studentId) {
-        enrolledstudentId = "";
+
+
+
+    public boolean addStudent(String studentId) {
+    if (currentlyEnrolled >= maxCapacity){
+        System.out.println("Courses is at maximum capacity! \n join the waitlist till a seat is empty or someone withdraws ");
+        return false;
+    } else if (enrolledStudents.contains(studentId)) {
+        System.out.println("You’re already enrolled in this course !");
+        return false;
     }
+    enrolledStudents.add(studentId);
+    currentlyEnrolled++;
+    return true ;
+
+    }
+
+
+    public boolean  removeStudent(String studentId) {
+        if (enrolledStudents.remove(studentId)){
+            System.out.println("You have been successfully withdrawn/removed from this course");
+            currentlyEnrolled--;
+            return true;
+
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    // che3cking if there is enough seats or if the course is full
+
+    public boolean isFull(){
+        return currentlyEnrolled >= maxCapacity;
+    }
+
     public int getAvailableSeats() {
-        return maxcapacity -1;
+        return maxCapacity - enrolledStudents ;
     }
+
+    // checking if the students prerequisites meet the prerequisites of the course
+
+    public boolean isPrerequisitesSatisfied (){
+        return true;
+    }
+
+    // display details of department
+
+    public void displayCourseDetails (){
+        System.out.println("Course details : \n");
+        System.out.println("Course code/ID : " + courseId );
+        System.out.println("Course code/ID : " + courseId );
+
+    }
+
 
 }
 
---
+++ project/src/University.java
@@ -0,0 +1,10 @@
+package project.src;
+
+import java.util.List;
+
+public class University {
+
+private String universityName;
+private List <Department> departments;
+private List <Course>
+}
